d2f1d3ba7a2ee4923028e1840f3588238e76d631
anuirsana <irsana98kt@gmail.com>
2023-08-11 18:38:24 +0530

    added

---
+++ ecommerceshopping/shoppingapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+
+from shoppingapp import views
+app_name='shoppingapp'
+urlpatterns = [
+    path('',views.allProductCategary,name='allproduct'),
+    path('<slug:c_slug>/',views.allProductCategary,name="product_by_categary"),
+    path('<slug:c_slug>/<slug:product_slug>/', views.proDetail, name="productdetail")
+    #path('',views.index,name='index'),
+
+]
